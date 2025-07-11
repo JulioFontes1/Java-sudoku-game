1b39f18aa10d91702165d3892bb05c4284c7dd03
JulioFontes1 <julio2006ofc@gmail.com>
2025-07-11 10:42:32 -0300

    Adicionar interface e regras de negócio para que o jogo seja solucionável

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,66 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final SudokuGenerator generator = new SudokuGenerator();
     public static void main(String[] args) {
-        SudokuGenerator generator = new SudokuGenerator();
+
         generator.generate();
+
+
+        while (true){
+            generator.printBoard();
+            System.out.println("Selecione a opção que deseja");
+            System.out.println("1 - Adcionar número");
+            System.out.println("2 - Remover número");
+            System.out.println("3 - Sair");
+            int option = scanner.nextInt();
+
+            switch (option){
+                case 1 -> AddNumber();
+                case 2 -> remNumber();
+                case 3 -> System.exit(0);
+            }
+        }
+    }
+
+    private static void AddNumber() {
+        while (true){
+            System.out.println("Selecione a opção que deseja");
+            System.out.println("Digite o número:");
+            int number = scanner.nextInt();
+            System.out.println("Digite a linha:");
+            int row = scanner.nextInt();
+            System.out.println("Digite a coluna:");
+            int coll = scanner.nextInt();
+
+            generator.addNumber(number, row, coll);
+            boolean verification = generator.isCompeted();
+            if (verification) {
+                gameCompleted();
+            }
+
+            return;
+        }
+    }
+
+    private static void remNumber(){
+        while (true){
+            System.out.println("Selecione a opção que deseja");
+            System.out.println("Digite a linha:");
+            int row = scanner.nextInt();
+            System.out.println("Digite a coluna:");
+            int coll = scanner.nextInt();
+
+            generator.remNumber(row, coll);
+            return;
+        }
+    }
+
+    private static void gameCompleted(){
+        System.out.println("Você completou o jogo");
         generator.printBoard();
+        System.exit(0);
     }
 
 
--- src/SudokuGenerator.java
+++ src/SudokuGenerator.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Random;
 
 public class SudokuGenerator {
+    private boolean startedGame = false;
     private int[][] board = new int[9][9];
     private int[][] hiddenBoard = new int[9][9];
+    private int[][] gameBoard = new int[9][9];
+    List<Integer> numbers;
     Random random = new Random();
 
     public int[][] generate(){
@@ -13,11 +16,20 @@ public class SudokuGenerator {
         return board;
     }
 
+    public boolean hasTable(){
+        if(!startedGame){
+            startedGame = true;
+            return false;
+        }
+
+        return true;
+    }
+
     private boolean fillBoard(){
         for (int row = 0; row < 9; row++) {
             for (int coll = 0; coll < 9; coll++) {
                 if(board[row][coll] == 0){
-                    List<Integer> numbers = getShuffledNumbers();
+                    numbers = getShuffledNumbers();
 
                     for (int num : numbers) {
                         if (isValid(row, coll, num)) {
@@ -26,6 +38,7 @@ public class SudokuGenerator {
                             board[row][coll] = 0;
                         }
                     }
+                    return false;
                 }
             }
         }
@@ -35,11 +48,10 @@ public class SudokuGenerator {
 
     private List<Integer> getShuffledNumbers() {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i <= 9; i++) list.add(i);
+        for (int i = 1; i <= 9; i++) list.add(i);
         Collections.shuffle(list);
         return list;
     }
-
 
     private boolean isValid(int row, int coll, int num){
         for (int c = 0; c < 9; c++) {
@@ -62,12 +74,27 @@ public class SudokuGenerator {
     }
 
     public void printBoard(){
-        hidden();
+        if(!hasTable()){
+            hidden();
+        }
         System.out.println("------------------------------");
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (col== 0) System.out.print(" | ");
-                System.out.print(hiddenBoard[row][col]+ " ");
+
+                int num = gameBoard[row][col];
+                String value;
+
+                if(num == 0){
+                    value = "  ";
+                } else if (hasDoubled(num, row, col)) {
+                    value = num + "*";
+                } else if(num == hiddenBoard[row][col]){
+                    value = String.valueOf(num) + " ";
+                }else {
+                    value = num + "_";
+                }
+                System.out.print(value);
                 if ((col + 1)%3 == 0) System.out.print(" | ");
             }
             System.out.println();
@@ -76,18 +103,84 @@ public class SudokuGenerator {
     }
     
     public void hidden(){
-        int max = 78;
-        int min = 70;
+        int max = 3;
+        int min = 1;
         int hidden = 0;
         int randomNumber = random.nextInt(max - min + 1) + min;
-        hiddenBoard = board;
+        hiddenBoard = copyBoard(board);
 
         while (hidden < randomNumber){
             int row = random.nextInt(9);
             int col = random.nextInt(9);
 
-            hiddenBoard[row][col] = 0;
-            hidden++;
+            if (hiddenBoard[row][col] != 0){
+                hiddenBoard[row][col] = 0;
+                hidden++;
+            }
         }
+        gameBoard = copyBoard(hiddenBoard);
+    }
+
+    public boolean isCompeted(){
+        int nCompleted = 0;
+        for (int row = 0; row < 9; row++) {
+            for (int coll = 0; coll < 9; coll++) {
+                if(gameBoard[row][coll] == board[row][coll]){
+                    nCompleted ++;
+                }
+            }
+        }
+
+        return nCompleted == 81;
+    }
+
+    public boolean hasDoubled(int num, int row, int coll){
+        for (int c = 0; c < 9; c++) {
+            if(c != coll && gameBoard[row][c] == num) return true;
+
+        }
+
+        for (int r = 0; r < 9; r++) {
+            if(r != row && gameBoard[r][coll] == num) return true;
+        }
+
+        int startRow = row - row % 3;
+        int startColl = coll - coll % 3;
+
+        for (int r = startRow; r < startRow + 3; r++) {
+            for (int c = startColl; c < startColl + 3; c++) {
+                if((r != row || c != coll) && gameBoard[r][c] == num) return true;
+            }
+        }
+        return false;
+    }
+
+    public void addNumber(int num, int row, int coll){
+        if(hiddenBoard[row][coll] != 0){
+            System.out.println("Não é permitido alterar um número colocado pelo jogo");
+            return;
+        }
+        gameBoard[row][coll] = num;
+        if(hasDoubled(num, row, coll)){
+            System.out.println("*" + num + ", linha:" + row + ", coluna:" + coll);
+        };
+    }
+
+    public void remNumber(int row, int coll){
+        if(hiddenBoard[row][coll] != 0){
+            System.out.println("Não é permitido alterar um número colocado pelo jogo");
+            return;
+        }
+        gameBoard[row][coll] = 0;
+    }
+
+    private int[][] copyBoard(int[][] source){
+        int[][] newBoard = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            System.arraycopy(source[i], 0, newBoard[i], 0, 9);
+        }
+
+        return newBoard;
     }
 }
